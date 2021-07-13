d1868b33808cfec1978c68f4fe2f86e4aba24ee8
Throde <1300864360@qq.com>
2021-07-13 22:00:38 +0800

    add job_test

---
+++ turngpt/job_test.py
@@ -0,0 +1,10 @@
+import pytorch
+import librosa
+
+print(">> import pytorch and librosa ok.")
+
+number = 3.14
+print(f">> f-print {number} ok")
+
+import time
+time.sleep(3)
--- turngpt/job_test.sh
+++ turngpt/job_test.sh
@@ -7,8 +7,8 @@
 #  memory limit of 1 Gbyte: -l h_vmem
 #$ -N turngpt             
 #$ -cwd
-#$ -l h_rt=48:00:00 
-#$ -l h_vmem=32G
+#$ -l h_rt=00:30:00 
+#$ -l h_vmem=1G
 
 # priority
 #$ -P lel_hcrc_cstr_students
@@ -21,16 +21,11 @@
 . /etc/profile.d/modules.sh
 
 # Load Python
-module load python/3.8  # 3.4.3
+# module load python/3.8  # 3.4.3
 module load anaconda
 source activate slptorch
 
 # Run the program
 # available datasets: taskmaster metalwoz multiwoz coached persona dailydialog
 # python hello.py
-python ./turngpt/main.py \
-  --model pretrained \
-  --datasets maptask switchboard \
-  --chunk_size 512 \
-  --gpus 1 \
-  --batch_size 2 \
+python ./job_test.py
