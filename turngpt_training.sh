46c0679e4cae253d9add7ebf75aada8cf0cb9cb2
Throde <33833556+Throde@users.noreply.github.com>
2021-07-13 19:07:23 +0800

    Update turngpt_training.sh
    fix prefix #$

--- turngpt_training.sh
+++ turngpt_training.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#  These options are:
+
 #  job name: -N
 #  use the current working directory: -cwd
 #  runtime limit of 5 minutes: -l h_rt
 #  memory limit of 1 Gbyte: -l h_vmem
-$ -N turngpt             
-$ -cwd
-$ -l h_rt=48:00:00 
-$ -l h_vmem=32G
+#$ -N turngpt             
+#$ -cwd
+#$ -l h_rt=48:00:00 
+#$ -l h_vmem=32G
 
--P lel_hcrc_cstr_students
+# priority
+#$ -P lel_hcrc_cstr_students
+
 # Email information
--M s2125085@ed.ac.uk
--m beas
+#$ -M s2125085@ed.ac.uk
+#$ -m beas
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
