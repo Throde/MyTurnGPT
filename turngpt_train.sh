014c8615854cc470148ddac5fd990133cbbbf410
Throde <1300864360@qq.com>
2021-08-08 18:17:19 +0800

    update train.sh

--- turngpt_train.sh
+++ turngpt_train.sh
@@ -34,7 +34,7 @@ source activate slptorch
 python ./turngpt/main.py \
   --model pretrained \
   --datasets taskmaster persona dailydialog maptask switchboard \
-  --chunk_size 128 \
+  --chunk_size 512 \
   --gpus 2 \
   --batch_size 1 \
   --num_workers 2 \
